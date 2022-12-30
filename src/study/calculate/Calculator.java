afa17e2a8a0fd80a98e003659b5ee4f640e54071
chanwoo <chocw0402@gmail.com>
2022-12-30 18:06:53 +0900

    feat : 숫자 계산기 클래스

---
+++ src/study/calculate/Calculator.java
@@ -0,0 +1,20 @@
+package study.calculate;
+
+public class Calculator {
+
+    static double add(double a, double b) {
+        return a + b;
+    }
+
+    static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    static double divide(double a, double b) {
+        return a / b;
+    }
+}
